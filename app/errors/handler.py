d22196d87fc1335aa9eccff848fc7d60963ab414
Neil Rutherford <neilinator@hotmail.com>
2020-08-04 22:06:00 -0600

    Error handler good to go

--- app/errors/handler.py
+++ app/errors/handler.py
@@ -2,13 +2,12 @@ from flask import render_template, current_app
 from app import db
 from app.errors import bp
 
-#current_app.config['TRAP_HTTP_EXCEPTIONS']=True
-#current_app.register_error_handler(Exception, defaultHandler)
-
+# Tested 2020-08-04
 @bp.app_errorhandler(404)
 def not_found_error(error):
     return render_template('errors/404.html'), 404
 
+# Tested 2020-08-04
 @bp.app_errorhandler(500)
 def internal_error(error):
     db.session.rollback()
