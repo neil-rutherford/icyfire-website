58d2be3b933279f1555a2e4814a64fa8d30d2ad5
Neil Rutherford <neilinator@hotmail.com>
2020-09-22 10:20:14 -0600

    Putting out fires from the depracation

--- yunti.py
+++ yunti.py
@@ -1,8 +1,8 @@
 from app import create_app, db
-from app.models import Domain, User, FacebookPost, FacebookCred, TwitterPost, TwitterCred, TumblrPost, TumblrCred, RedditPost, RedditCred, TimeSlot, Sentry, CountryLead, RegionLead, TeamLead, Agent
+from app.models import Domain, User, FacebookPost, FacebookCred, TwitterPost, TwitterCred, TumblrPost, TumblrCred, RedditPost, RedditCred, TimeSlot, Sentry, Partner #CountryLead, RegionLead, TeamLead, Agent
 
 app = create_app()
 
 @app.shell_context_processor
 def make_shell_context():
-    return {'db': db, 'User': User, 'Domain': Domain, 'FacebookPost': FacebookPost, 'FacebookCred': FacebookCred, 'TwitterPost': TwitterPost, 'TwitterCred': TwitterCred, 'TumblrPost': TumblrPost, 'TumblrCred': TumblrCred, 'RedditPost': RedditPost, 'RedditCred': RedditCred, 'TimeSlot': TimeSlot, 'Sentry': Sentry, 'CountryLead': CountryLead, 'RegionLead': RegionLead, 'TeamLead': TeamLead, 'Agent': Agent}
+    return {'db': db, 'User': User, 'Domain': Domain, 'FacebookPost': FacebookPost, 'FacebookCred': FacebookCred, 'TwitterPost': TwitterPost, 'TwitterCred': TwitterCred, 'TumblrPost': TumblrPost, 'TumblrCred': TumblrCred, 'RedditPost': RedditPost, 'RedditCred': RedditCred, 'TimeSlot': TimeSlot, 'Sentry': Sentry, 'Partner': Partner} #'CountryLead': CountryLead, 'RegionLead': RegionLead, 'TeamLead': TeamLead, 'Agent': Agent}
